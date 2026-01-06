3c7f364d78b807c19c205130b03094f4974f2446
Shoaib Taimur <mitashoaib@gmail.com>
2026-01-07 00:49:03 +0600

    Added service section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import { useLoaderData } from 'react-router';
 import DocContainer from '../Doclist/DocContainer';
+import Services from '../Services/Services';
 
 const Home = () => {
     const docInfoData=useLoaderData();
@@ -10,6 +11,7 @@ const Home = () => {
         <div className='bg-[#ebebeb] space-y-15 mt-6 mb-6'>
             <Banner></Banner>
             <DocContainer docInfoData={docInfoData}></DocContainer>
+            <Services></Services>
         </div>
     );
 };
